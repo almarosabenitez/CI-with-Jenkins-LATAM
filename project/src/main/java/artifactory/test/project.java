51233ca82884ceb0e8ef4173848b4f87cc7d1680
RAMKITCS <ramki.veda@gmail.com>
2018-05-09 19:04:19 +0530

    Create project.java

---
+++ project/src/main/java/artifactory/test/project.java
@@ -0,0 +1,11 @@
+package artifactory.test;
+
+/**
+ * Hello world!
+ */
+public class Project {
+    public static void main(String[] args) {
+        new Project();
+        System.out.println("Hello World!");
+    }
+}
